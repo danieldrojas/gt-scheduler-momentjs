72e7cda0cfbc116fede12b16c10ffbcebfc7a269
Daniel Rojas <daniel.drojas@outlook.com>
2020-06-15 20:28:19 -0400

    added icon

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function(){  
     
-    console.log("Inside jquery function");
-
-    //Variables
-    // var column3 = $("<button>")
-    //     .addClass("col-sm-1 saveBtn")
-    //     .html("<i class='fas fa-save'></i>");
-    // var time = {
-    //     txtHours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
-    // }
-    // timeIndex = 0;
 
 
-
-    //Set content
+//Add the time in /MM/DD/YYYY format
     $("#currentDay").text(moment().format('MMMM Do YYYY'));
 
-    // var colTime = $("<div>");
-    // colHour.addClass(".hour");
-     
-    console.log('THis moment .hours!!', moment().format('h'), moment().hours());   // 7:21 PM
+
+    //LocalStorage Function
+    function fillInTextArea() { 
+        for (var i = 9; i < 18; i++){
+       var hour = i
+            if (i > 12) {  
+                hour = i - 12 //subtract 12 to have 12 hours format 
+                
+            }
+            var text = localStorage.getItem(hour);
+            $("#input-" + hour).val(text)
+        }
+    }
 
 
-    //timeblocks 
+    //Dynamically generate rows for timeblocks
     for (var i = 9; i < 18; i++){
-        var currentHour = 13
+        var currentHour = moment().hours();
         var hour = i
-        var timeCode = 'AM'
+        var timeMeridiem = 'AM'
         var scheduleColorClass = ''
 
         
         if (i > 12) { 
             hour = i - 12
-            timeCode = 'PM'
+            timeMeridiem = 'PM'
         }
 
         if (i === currentHour) { 
             scheduleColorClass = 'present'
-
         } else if (i < currentHour) {
             scheduleColorClass = 'past'
         } else if (i > currentHour) { 
@@ -45,63 +42,21 @@ $(document).ready(function(){
         }
 
 
-        $(".container").append($(`<div class='row time-block ${scheduleColorClass}'>` + hour + timeCode + "<div class='col-8 alert' >" + `<textarea id=${'input-' + hour}></textarea>` + `<button id=${hour} class='btn saveBtn' style='float: right'>` + 'sve img' + "</button></div>"));
+        $(".container").append($(`<div class='row time-block'>` + `<span class='hour col-1'>` + hour + timeMeridiem + `</span >` + `<textarea class="col-10 ${scheduleColorClass}"  id=${'input-' + hour}></textarea>` + `<button id=${hour} class='btn fa fa-save col-1 saveBtn'>` + `</button>`));
     }
-     
 
+
+    fillInTextArea()
+     
+//Event Listener 
     $(".saveBtn").on("click", function () {
         console.log("click", $(this).attr("id"))
         console.log('what we typed!!', $('#input-' + $(this).attr("id")).val())
         var storeActivities = $(this).attr("id");
-        localStorage.setItem(storeActivities, $('#input-' + $(this).attr("id")).val())    
+        localStorage.setItem(storeActivities, $('#input-' + $(this).attr("id")).val())
+        arrayActivities.push($('#input-' + $(this).attr("id")).val());
 
-    })
-
-
-    
-    // way to organize thing sin the localstorage use this structure!!
-    //[{ 9: 'hello' }, { 10: 'yess' }, {11: 'lunch'}]
-    //[{9: 'yesss'}]
-    //JSON.stringify()
-    //JSON.parse()
-
-
-
-
-    
-    // <div class="row">
-    //     <div class="col">
-    //         <div id="timeblock" class="alert alert-warning" role="text">
-    //             <strong>Holy guacamole!</strong> You should check in on some of those fields
-    //                 below.
-    //                 <button class="btn btn-primary" style="float: right" >save pic</button>
-
-    //         </div>
-    //     </div>
-    // </div>
-
-    // for (var i = 0; i < 18; i++) {
-    //     $(".container").append($("<div class='row'>" + i + 'AM' + "<div class='col-8 alert alert-warning' >" + 'activities goes in here' + "<button class='btn bsaveBtn' style='float: right'>" + 'sve img' + "</button></div>"));
-    // }
-
-  
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    });
 
 
 });
